56796754f4907455b675de0991fdcbf71d330ebe
Kuzya <116298294+Silent128-68@users.noreply.github.com>
2024-03-03 20:07:11 +0400

    Create MyForm.cpp

---
+++ 3laba/MyForm.cpp
@@ -0,0 +1,18 @@
+#include "Matrix.h"
+#include "Transform.h"
+#include "Figure.h"
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Tishchenko::MyForm form;
+	Application::Run(% form);
+}
